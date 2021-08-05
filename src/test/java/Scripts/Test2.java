5d84a6a376a7c6a3362eb4e893aa0c4ea018f6de
Sundar <Sundar@DESKTOP-RNGP1V9>
2021-08-05 18:58:01 +0530

    hello

---
+++ src/test/java/Scripts/Test2.java
@@ -0,0 +1,14 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void demo() {
+		
+		Reporter.log("Hello Appium",true);
+		
+	}
+
+}

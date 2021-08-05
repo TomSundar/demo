67980c7175d7db7e600a7a5549f6d51d0b33dec1
Sundar <Sundar@DESKTOP-RNGP1V9>
2021-08-05 18:18:15 +0530

    hi

---
+++ src/test/java/Scripts/Test0.java
@@ -0,0 +1,14 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	@Test
+	public void demo() {
+		
+		Reporter.log("Hello Java",true);
+		
+	}
+
+}
